a76530bb062687c3c8f57596a100a39c1a7e54a7
yusuke-hada <aspringwithoutlife4929@gmail.com>
2025-03-17 11:26:52 +0900

    feat_初期クラス追加

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+public class HelloWorld {
+
+    static String a = "Hello World!";
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(a);
+	}
+}
